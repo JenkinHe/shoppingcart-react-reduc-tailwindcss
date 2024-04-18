bd234f580ae017d04a5490cc755220eab3100477
Jenkibear <jenkinwork123@gmail.com>
2024-04-18 20:33:53 +1000

    indivudal items taken from api and mapped to home screen with tailwind css design

--- shopping-cart-app/src/pages/home.jsx
+++ shopping-cart-app/src/pages/home.jsx
@@ -1,38 +1,47 @@
 import { useEffect, useState } from "react"
-import {Circles} from 'react-loader-spinner';
+import { Circles } from 'react-loader-spinner';
+import ProductTile from "../components/product-tile";
 
 
 
-export default function Home(){
-    const [products,setProducts] =useState([]);
-    const [loading,setLoading] = useState(false);
+export default function Home() {
+    const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(false);
 
-    async function fetchListOfProducts(){
+    async function fetchListOfProducts() {
         setLoading(true);
-        const response =await fetch('https:fakestoreapi.com/products');
-        const data= await response.json();
+        const response = await fetch('https:fakestoreapi.com/products');
+        const data = await response.json();
 
-        if(data){
+        if (data) {
             setLoading(false);
             setProducts(data);
         }
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         fetchListOfProducts();
-    },[])
+    }, [])
 
     return <div>
         {
-            loading?
-            <div className="min-h-screen w-full flex justify-center items-center">
-                <Circles
-                height={'120'}
-                width={'120'}
-                color="rgb(127,29,29)"
-                visible={'true'}
-                />
-            </div>:null
+            loading ?
+                <div className="min-h-screen w-full flex justify-center items-center">
+                    <Circles
+                        height={'120'}
+                        width={'120'}
+                        color="rgb(127,29,29)"
+                        visible={'true'}
+                    />
+                </div> :
+                <div className="min-h-[80vh] grid sm:grid-cols-2 md:grid-cols-3 space-x-5 space-y-10 lg:grid-cols-4 max-w-6xl mx-auto p-3">
+                    {
+                        products && products.length ?
+                            products.map(productItem => <ProductTile product={productItem} />)
+                            : null
+                    }
+
+                </div>
         }
     </div>
 }
