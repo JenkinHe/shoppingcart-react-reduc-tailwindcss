4c90b79296be89bb084cb0f90de8aa8680a61508
Jenkibear <jenkinwork123@gmail.com>
2024-04-18 21:20:36 +1000

    rendered empty cart redirect to home page

--- shopping-cart-app/src/pages/cart.jsx
+++ shopping-cart-app/src/pages/cart.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
-import { useSelector } from "react-redux"
+import { useSelector } from "react-redux";
+import {Link} from 'react-router-dom';
 
 
 
@@ -13,5 +14,21 @@ export default function Cart(){
         setTotalCart(cart.reduce((acc,curr)=>acc+curr.price,0));
     },[cart])
 
-    return <div>cart</div>
+    return <div>
+    {
+        cart&&cart.length?
+        <div>
+
+        </div>:
+        <div className="min-h-[80vh] flex flex-col items-center justify-center">
+            <h1 className="text-gray-800 font-bold text-xl mb-2">Your cart is Empty</h1>
+            <Link to='/'>
+                <button className="bg-red-950 text-white border-2 rounded-lg font-bold p-4">
+                    Shop Now
+                </button>
+
+            </Link>
+        </div>
+    }
+    </div>
 }
