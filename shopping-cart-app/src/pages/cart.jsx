d3db00456527b0d9f96553fce4fea99ce779bcc8
Jenkibear <jenkinwork123@gmail.com>
2024-04-18 21:37:41 +1000

    made and styled cart and also able to remove items from cart when in cart

--- shopping-cart-app/src/pages/cart.jsx
+++ shopping-cart-app/src/pages/cart.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import {Link} from 'react-router-dom';
+import CartTile from "../components/cart-tile";
 
 
 
@@ -14,10 +15,28 @@ export default function Cart(){
         setTotalCart(cart.reduce((acc,curr)=>acc+curr.price,0));
     },[cart])
 
-    return <div>
+    return <div className="flex justify-center">
     {
         cart&&cart.length?
-        <div>
+        <div className="min-h-[80vh] grid md:grid-cols-2 max-w-6xl mx-auto">
+            <div className="flex flex-col justify-center items-center p-3">
+                {
+                    cart.map(cartItem=><CartTile cartItem={cartItem}/>)
+                }
+            </div>
+            <div className="w-[300px]">
+                <div className="flex flex-col justify-center items-end p-5 space-y-5 mt-14">
+                    <h1 className="font-bold text-lg text-red-800"> Your Cart Summary</h1>
+                    <p>
+                        <span className="text-gray-800 font-bold">Total Item</span>
+                        <span>:{cart.length}</span>
+                    </p>
+                    <p>
+                        <span className="text-gray-800 font-bold">Total Amount</span>
+                        <span>:{totalCart}</span>
+                    </p>
+                </div>
+            </div>
 
         </div>:
         <div className="min-h-[80vh] flex flex-col items-center justify-center">
